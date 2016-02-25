25343f5298abd2ce7640cbd2b4df6de395a6415e
Quentin Renard <contact@asticode.com>
2016-02-25 21:20:52 +0100

    goimports

--- cachemanager/handler_memory_test.go
+++ cachemanager/handler_memory_test.go
@@ -1,7 +1,9 @@
 package cachemanager
+
 import (
 	"testing"
 	"time"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -22,7 +24,7 @@ func TestHandlerMemory(t *testing.T) {
 	m := NewHandlerMemoryFromConfiguration(c)
 
 	// Set
-	m.Set(k, v, time.Duration(5) * time.Microsecond)
+	m.Set(k, v, time.Duration(5)*time.Microsecond)
 	vc, e := m.Get(k)
 	assert.NoError(t, e)
 	assert.Equal(t, v, vc)
@@ -33,13 +35,13 @@ func TestHandlerMemory(t *testing.T) {
 	assert.EqualError(t, e, ErrCacheMiss.Error())
 
 	// Del
-	m.Set(k, v, time.Duration(5) * time.Microsecond)
+	m.Set(k, v, time.Duration(5)*time.Microsecond)
 	m.Del(k)
 	vc, e = m.Get(k)
 	assert.EqualError(t, e, ErrCacheMiss.Error())
 
 	// Increment
-	m.Set(k, uint64(5), time.Duration(20) * time.Microsecond)
+	m.Set(k, uint64(5), time.Duration(20)*time.Microsecond)
 	_, e = m.Increment(k, 1)
 	assert.NoError(t, e)
 	vc, e = m.Get(k)
